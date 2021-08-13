c8f428c91c7e271ebf9b0d0c9b0ebd1560cd35bd
rayshell <a02003972@utmir.edu.mx>
2021-08-13 15:53:06 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Webpack-starter
+Proyecto incial para crear aplicaciones web con Webpack
+### Notas:
+Recuerde contruir los modulos de Node
+```
+npm install
+```
+Y para construir la carpeta dist
+```
+npm run build
+```
+Para el ambiente de desarrollo
+```
+npm run build:dev
+```
---
+++ webpack.prod.js
@@ -0,0 +1,76 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizer = require("css-minimizer-webpack-plugin");
+const Terser = require("terser-webpack-plugin");
+const { loader } = require('mini-css-extract-plugin');
+
+module.exports = {
+
+    mode: 'production',
+    output:{
+        clean:true,
+        filename: 'main.[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                },
+            },
+            {
+                test: /\.css$/i,
+                exclude: /styles.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use:[MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            ]
+            },
+    optimization : {
+        minimizer: true,
+        minimizer: [
+            new CssMinimizer(),
+            new Terser(),
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            title: 'Webpack app'
+        }),
+        new MiniCssExtract({
+            filename: '[name].[fullhash].css',
+            ignoreOrder: false,
+        }),
+        new MiniCssExtract({
+            filename: 'style.css',
+            ignoreOrder: false,
+        }),
+        new CopyPlugin({
+            patterns:[
+                {from:'src/assets/', to:'assets/'}
+            ]
+        }),
+    ]
+}
